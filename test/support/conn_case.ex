7a21ec617081c5e6d6e42a9acebb2685bef3bb7a
Jake Morrison <jake@cogini.com>
2021-10-27 22:04:32 +0800

    Import Phoenix.ConnText instead of use

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -6,7 +6,8 @@ defmodule ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
     end
   end
 
