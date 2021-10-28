66693f434e22a75ecb7c574407166a0da4bd0e88
Jake Morrison <jake@cogini.com>
2021-10-28 14:11:25 +0800

    Set JSON library for phoenix

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,8 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :phoenix, :json_library, Jason
+
 config :authtoken,
   token_key: <<1, 2, 3, 230, 103, 242, 149, 254, 4, 33, 137, 240, 23, 90, 99, 250>>
 
